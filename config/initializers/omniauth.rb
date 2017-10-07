06d56a9c919a8eaa9280d29b9ca8704ad9b39810
Tanja Stroble <plusdeltah@gmail.com>
2017-10-07 00:05:00 -0700

    models,initializers and controllers

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,11 @@
+# app keys and secrets go here
+
+
+
+
+#Rails.application.config.middleware.use OmniAuth::Builder do
+	#provider :twitter, 'Consumer Key (API Key)', 'Consumer Secret (API Secret)'
+	#provider :facebook, 'App ID', 'App Secret'
+	#provider :github, 'Client ID', 'Client Secret
+#'
+#end
